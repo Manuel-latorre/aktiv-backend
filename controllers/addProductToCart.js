326a37cd25748c1b1efe917ad6fb81c4c8f932d2
Manuel-latorre <manuel.latorre11@gmail.com>
2024-03-11 16:15:20 -0300

    add id in addproduct

--- controllers/addProductToCart.js
+++ controllers/addProductToCart.js
@@ -2,24 +2,24 @@ const Cart  = require('../models/cart');
 const Product = require('../models/products');
 
 const addProductCart  = async (req, res) => {
-    const {name, imageCard, price} = req.body;
+    const {name, imageCard, price, id_} = req.body;
 
-    const ifExist = await Product.findOne({name});
+    const ifExist = await Product.findOne({id_});
 
-    const ifEmpty = name !== "" && imageCard !== "" && price !== "";
+    const ifEmpty = name !== "" && imageCard !== "" && price !== "" && id_ !== "";
 
-    const ifStayIncart = await Cart.findOne({name});
+    const ifStayIncart = await Cart.findOne({id_});
 
     if(!ifExist){
         res.status(400).json({
             mensaje: "This product is not available",
         });
     }else if (ifEmpty && !ifStayIncart){
-        const newProdInCart = new Cart({name, imageCard, price, quantity: 1});
+        const newProdInCart = new Cart({id_, name, imageCard, price, quantity: 1});
 
         await Product.findByIdAndUpdate(
             ifExist?._id,
-            {inCart: true, name, imageCard, price},
+            {inCart: true, name, imageCard, price, id_},
             {new: true}
         )
 
