9393f21819af802c50cbeb947fb7a4a82a9e908d
Manuel-latorre <manuel.latorre11@gmail.com>
2024-03-12 11:41:30 -0300

    add size in delete prod

--- controllers/deleteProductCart.js
+++ controllers/deleteProductCart.js
@@ -6,14 +6,14 @@ const deleteProductCart = async (req, res) => {
 
     const ifStayInCart = await Cart.findById(productId);
 
-    const {name, imageCard, price, _id} = await Product.findOne({
+    const {name, imageCard, price, size, _id} = await Product.findOne({
         name: ifStayInCart.name,
     });
 
     await Cart.findByIdAndDelete(productId);
     await Product.findByIdAndUpdate(
         _id,
-        {inCart: false, name, imageCard, price},
+        {inCart: false, name, imageCard, price, size},
         {new: true}
     )
 
